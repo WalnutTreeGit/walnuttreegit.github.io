96304e37d829b7a774ccb192a082c86370e1cd36
WalnutTreeGit <110934329+WalnutTreeGit@users.noreply.github.com>
2026-02-04 12:47:45 +0000

    Deploying to gh-pages from @ WalnutTreeGit/walnuttreegit.github.io@c17709b426292be9a841ab620e6a487e82d0dff6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,22 +409,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-real-time-facial-recognition",
+          section: "News",},{id: "projects-real-time-facial-recognition",
           title: 'Real-Time Facial Recognition',
           description: "A closed-set facial recognition pipeline using YOLOv8 and Transfer Learning.",
           section: "Projects",handler: () => {
