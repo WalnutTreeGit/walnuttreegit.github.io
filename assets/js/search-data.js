da36142cc37e4a4c3dc37d0d6a0b1f124e09f9cc
WalnutTreeGit <110934329+WalnutTreeGit@users.noreply.github.com>
2026-02-06 00:54:43 +0000

    Deploying to gh-pages from @ WalnutTreeGit/walnuttreegit.github.io@9895a7e4f765a8c58f1e21ad2a47234902812ea5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,8 +424,8 @@ ninja.data = [{
           description: "Solver-backed learning of k-CNF formulas mixing Boolean literals and linear real arithmetic halfspaces (PySMT).",
           section: "Projects",handler: () => {
               window.location.href = "/projects/smt/";
-            },},{id: "projects-real-time-train-crowding-forecasting-sncf-transilien",
-          title: 'Real-Time Train Crowding Forecasting (SNCF-Transilien)',
+            },},{id: "projects-real-time-train-crowding-forecasting",
+          title: 'Real-Time Train Crowding Forecasting',
           description: "Tabular ML forecasting of one-station-ahead train occupancy using contextual + lag features from infrared door sensors.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/sncf-ml/";
